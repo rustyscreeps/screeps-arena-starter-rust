19235b508069708deefa16db5fc3f37ad472ef20
Shane Madden <shanemadden@users.noreply.github.com>
2022-09-26 08:05:53 -0600

    Improve import syntax to correctly import named export

--- javascript/main.mjs
+++ javascript/main.mjs
@@ -1,4 +1,4 @@
-import initSync from "./screeps-arena-starter-rust";
+import { initSync } from "./screeps-arena-starter-rust";
 import wasm_bytes from "./screeps-arena-starter-rust_bg.wasm.bin";
 initSync(wasm_bytes);
 export * from "./screeps-arena-starter-rust";
